7db7cbfb0c95b7858099fb30aef11f6c1ccc2776
PkSmart <p1ooooo100@gmail.com>
2024-05-10 23:52:42 +0530

    site made responsive

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -7,7 +7,7 @@ import NewsletterSection from "../Components/Newsletter";
 
 const Shop = () => {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 px-7">
       <Hero />
       <Popular />
       <Offers />

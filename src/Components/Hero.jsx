e37383a75e29b6507261bfc0877c224688dbd4dd
pradeep <pradeepbisen60@gmail.com>
2024-05-16 01:00:00 +0530

    Revert "added carousel on home page"
    This reverts commit 5b7ec4b7cf286c5ac41fed0f46793a218ca980b9.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,43 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import HeroImg from "../assets/hero2.jpg";
-import HeroImg2 from "../assets/hero1.jpg";
-import HeroImg3 from "../assets/Hero.jpg";
 import arrow from "../assets/arrow.png";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 const Hero = () => {
-  const data = [
-    {
-      img:HeroImg,
-      index: 1,
-    },
-    {
-      img:HeroImg2 ,
-      index: 2,
-    },
-    {
-      img: HeroImg3,
-      index: 3,
-    },
-  ];
-
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 1500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    focusOnSelect: true,
-
-  };
   return (
     <div className="w-full z-10 relative">
-      <div className="flex z-20 relative items-center flex-col">
-        <p className="text-sm md:text-base lg:text-lg   font-Poppins text-center text-black pt-10 md:pt-20 absolute top-28 left-0 md:left-[250px]">
+      <div className="flex items-center flex-col">
+        <p className="text-sm md:text-base lg:text-lg font-Poppins text-center text-black pt-10 md:pt-20 absolute top-28 left-0 md:left-[250px]">
           SPRING/SUMMER COLLECTION 2021
         </p>
         <p className="text-2xl md:text-4xl lg:text-5xl font-Poppins text-center text-black pt-16 md:pt-32 absolute top-32 left-0 md:left-[250px]">
@@ -51,27 +20,7 @@ const Hero = () => {
         </button>
       </div>
 
-      {/* <img src={HeroImg} alt="image" /> */}
-      <Slider {...settings}>
-       {/* <Slider {...settings}> */}
-     
-        {data.map((d, index) => (
-          <div
-            key={index}
-            className="bg-white z-0  shadow-md gap-2 p-1  text-black"
-          >
-            <div className=" mt-14 rounded-t-xl px-16 flex justify-center p-2 items-center  ">
-              <img src={d.img} className=" h-96 w-full" alt="kkk" />
-            </div>
-            {/* <div className="flex-col flex bg-white font-semibold justify-center items-center gap-2 p-4 ">
-                <p className="text-lg  font-semibold ">{d.Name}</p>
-                <p>{d.description}</p>
-              </div> */}
-          </div>
-        ))}
-        {/* </Slider> */}
-        </Slider>
-      
+      <img src={HeroImg} alt="image" />
     </div>
   );
 };
